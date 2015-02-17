e0a1296f5f6dc6ba3ee98d0c41d2c96d02d0b2d8
rakwaht <Martintoni@MacBook-Pro-di-Martintoni.local>
2015-02-17 13:39:11 +0100

    BAG detection

--- main.m
+++ main.m
@@ -8,8 +8,10 @@ clear all; close all;clc;
 start_time = cputime;
 
 %we need JPEG image I suppose
-file_name = 'DataHiding2014Project/img/lena.tif';
-img       = imread(file_name);
+file_name = 'DataHiding2014Project/img/tulipano10.jpg';
+x       = imread(file_name);
+img = rgb2gray(x);
+
 [imH imW] = size(img);
 
 %% Exclude edges. For preserving BAGs we only excluded edges within certain range.
@@ -23,7 +25,7 @@ edges_img = edge(img,'sobel');
 %figure; imshowpair(Gmag, Gdir, 'montage');
 % Then we defined whether a pixel is excluded R=1 means excluded
 R = zeros(imH, imW);
-theta = 0.1;
+theta = 1;
 % Define R using formula (1) of the paper
 for i = 1:imH*imW
     if ((Gmag(i) > 0 && Gmag(i) < theta) || ((Gmag(i) > (pi/2 - theta)) && (Gmag(i) < (pi/2 + theta))) || ((Gmag(i) > (pi - theta)) && (Gmag(i) < pi + theta)))
@@ -42,6 +44,51 @@ for i = 2:imH-1
         d(i,j) = abs(2*img(i,j) - img(i-1,j) - img(i+1,j));
     end
 end
+
+a = zeros(imH, imW);
+ar = zeros(imH, imW);
+b = zeros(imH, imW);
+
+for i = 1:imH
+    for j = 17:imW
+        for k = 1:16
+            if(R(i,j) == 0 && d(i,j) < 0.1)
+                a(i,j) = a(i,j) + d(i,k);
+            end
+        end
+    end
+end
+
+med = medfilt2(a, [16 1]);
+
+for i = 1:imH
+    for j = 1:imW
+        ar(i,j) = a(i,j) - med(i,j);
+    end
+end
+
+%imshow(ar);
+
+for i = 17:imH-17
+    for j = 17:imW-17
+        temph = medfilt1(ar([i-16 i-8 i i+8 i+16],j),5);
+        tempv = medfilt1(ar(i,[j-16 j-8 j j+8 j+16]),5);
+        b(i,j) = max(temph) + max(tempv);
+    end
+end
+
+%imshow(b);
+
+%% Noise estimation
+H = zeros(imH, imW);
+for i = 1:imH*imW
+    if(edges_img(i) ~= 0)
+        H(i) = 1;
+    end
+end
+
+V = ones(imH, imW);
+Z = xor(H, V);
 
 %% print the images
 %subplot(1,2,1); imshow(img);
